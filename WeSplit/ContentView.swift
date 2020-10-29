a8ed6fbdd1ddb5d847388ecf9adcdcb86453a107
BIll Ting <bting@lcs.on.ca>
2020-10-29 19:27:02 -0400

    Started WeSplit

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+         Form {
+          Section{
+            Text("Hello, world!")
+            Text("Hello, world!")
+              }
+          Section {
+            Text("Hello, world!")
+            Text("Hello, world!")
+               }
+            }
+        }
     }
 }
 
@@ -19,3 +29,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+ 
